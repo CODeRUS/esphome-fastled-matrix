a03a71f896ec72eef60819e4795a69782f56a026
necroave <55869272+necroave@users.noreply.github.com>
2021-04-15 20:10:23 +0300

    Update light.py

--- custom_components/fastled_matrix_clockless/light.py
+++ custom_components/fastled_matrix_clockless/light.py
@@ -89,6 +89,7 @@ def new_fastled_matrix(config):
     cg.add_library('https://github.com/marcmerlin/FastLED_NeoMatrix.git', '')
     cg.add_library('https://github.com/marcmerlin/Framebuffer_GFX.git', '')
     cg.add_library('Adafruit GFX Library', '')
+    cg.add_library('Adafruit BusIO', '')    
     cg.add_library('SPI', '')
     cg.add_library('Wire', '')
     yield var
